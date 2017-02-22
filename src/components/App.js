d2ff594aa0b6013a3221e96f326c779b5d77f0b0
Andrew Crist <andrew.l.crist@gmail.com>
2017-02-21 18:32:05 -0700

    [AC] add dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from '../styles/images/logo.svg';
+import logo from '../styles/images/logo.png';
 import '../styles/css/App.css';
+import Dashboard from './Dashboard'
+import moment from 'moment'
 
 class App extends Component {
   render() {
@@ -9,10 +11,9 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Bird's Eye Community</h2>
+          <p>{moment().format('MMMM Do YYYY')}</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Dashboard />
       </div>
     );
   }
