9c8e26152534b00a21b56e7b2fef6f929dc94954
Andrew Crist <andrew.l.crist@gmail.com>
2017-02-21 20:19:49 -0700

    AC add firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from '../styles/images/logo.png';
 import '../styles/css/App.css';
 import Dashboard from './Dashboard'
+import Login from './Login'
+import SignOut from './SignOut'
 import moment from 'moment'
+import { split } from 'lodash';
+
 
 class App extends Component {
+    constructor(){
+    super()
+    this.state = {
+      user: null
+    }
+  }
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => this.setState({ user }));
+  }
+
   render() {
+    let { user } = this.state
+    let currentUser;
+    let firstName;
+    if (user !== null) {
+      currentUser = user.displayName
+      firstName = split(user.displayName, ' ')
+    }
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Bird's Eye Community</h2>
-          <p>{moment().format('MMMM Do YYYY')}</p>
+      {!this.state.user ?
+        <Login />
+        :
+        <div className="app-container">
+          <div className="App-header">
+            <div className="app-logo-container">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Bird's Eye Community</h2>
+              <p>{moment().format('MMMM Do YYYY')}</p>
+            </div>
+            <div className="app-user">
+              <p>Welcome {currentUser}</p>
+              <SignOut />
+            </div>
+          </div>
+          <Dashboard />
         </div>
-        <Dashboard />
+      }
       </div>
     );
   }
