a9d24baa366ed28e5eb920be0f6512c7b3182364
KINAN WHYTE <kinanwhyte@gmail.com>
2017-02-26 18:46:30 -0700

    [FIX]: Fix merge conflicts with SignIn.jsx

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { signIn } from '../firebase'
+import { split } from 'lodash';
 
 const SignIn = ({ user }) => {
 
   return (
-    <div className="login-container">
-        <button className='sign-in-btn' onClick={() => signIn()}>Click to sign in with Google</button>
+    <div className="sign-in-container">
+      <button className='sign-in-btn' onClick={() => signIn()}>Click to sign in with Google</button>
     </div>
   );
 };
