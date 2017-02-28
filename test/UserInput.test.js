961a07716d971579c7805afefcac191a5dd5ad7a
KINAN WHYTE <kinanwhyte@gmail.com>
2017-02-27 18:54:36 -0700

    [ADD]: Add test suite for UserInput Component

---
+++ test/UserInput.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { shallow, mount, render } from 'enzyme'
+import { assert } from 'chai'
+
+import UserInput from '../src/components/UserInput.jsx'
+
+const sinon = require('sinon')
+
+describe('Unit Test | UserInput', () => {
+  it('can mount with no properties', () => {
+    const wrapper = shallow(<UserInput />)
+  })
+
+  it('renders as a <form>', () => {
+    const wrapper = shallow(<UserInput />)
+    assert.equal(wrapper.type(), 'form')
+  })
+
+  it('renders xml elements', () => {
+    sinon.spy(UserInput.prototype, 'render')
+    const wrapper = mount(<UserInput />)
+    assert.equal(UserInput.prototype.render.calledOnce, true)
+  })
+
+  it('should have a form element with 1 prop', () => {
+    const wrapper = render(<UserInput />)
+    assert.equal(wrapper.find('.message-input').length, 1)
+  })
+
+  it('should have a button with 1 prop', function(){
+    const wrapper = render(<UserInput />)
+    assert.equal(wrapper.find('.submit-button').length, 1)
+  })
+})
